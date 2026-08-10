a23433c348999a14ca75868c2bfd81ec47348a32
Md. Mohiuddin Reyad <65802483+Reyad07@users.noreply.github.com>
2026-08-10 15:22:22 +0600

    Add testbench for 4-tap FIR filter

---
+++ sim/fir_4tap_tb.sv
@@ -0,0 +1,37 @@
+module fir_4tap_tb;
+    
+    localparam WIDTH = 8;
+    
+    logic clk_i = '0;
+    logic arst_n;
+    logic sample_en;
+    logic [WIDTH-1:0] x_i;
+    logic [WIDTH-1:0] y_o;
+        
+   fir_4tap #(
+        .WIDTH(WIDTH)
+    ) u_fir_4tap (
+        .clk_i      (clk_i),
+        .arst_n     (arst_n),
+        .sample_en  (sample_en),
+        .x_i        (x_i),
+        .y_o        (y_o)
+    );
+    
+    always #5 clk_i = ~clk_i;
+    
+    initial begin
+        arst_n <= 1'b0;
+        sample_en <= 1'b0;
+        repeat(5) @(posedge clk_i);
+        arst_n <= 1'b1;
+        
+        sample_en <= 1'b1;
+        repeat (10000) begin
+            @(posedge clk_i);
+            x_i <= $random;
+        end
+    end
+    
+endmodule
+
